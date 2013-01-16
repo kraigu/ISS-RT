7c306ee08041ad7e7396bbd9451800a940959ea3
Mike Patterson <mike@snowcrash.ca>
2013-01-16 11:34:22 -0500

    incident submit: take a risk vector as optional third argument

--- bin/rt-incident-submit.pl
+++ bin/rt-incident-submit.pl
@@ -13,6 +13,7 @@ use RT::Client::REST::Ticket;
 use Error qw|:try|;
 use Date::Manip;
 use ConConn;
+use Scalar::Util qw(looks_like_number);
 
 my $debug = 0;
 
@@ -20,6 +21,12 @@ my %config = ISSRT::ConConn::GetConfig();
 
 my $subject = $ARGV[0] || die "Need a subject\n";
 my $infilename = $ARGV[1] || die "Need a file name\n";
+my $pri;
+if($pri = $ARGV[2]){
+	die "Priority must be a digit (1-5)\n" unless looks_like_number($pri);
+} else {
+	$pri = 3;
+}
 $/ = undef; # we're going to want to read a whole file into a string
 open(FILE,"$infilename") || die "Couldn't open file $infilename $!\n";
 my $rttext = <FILE>;
@@ -40,6 +47,9 @@ my $ticket = RT::Client::REST::Ticket->new(
 	rt => $rt,
 	queue => "Incidents",
 	subject => $subject,
+	cf => {
+		'Risk Severity' => $pri
+	},
 )->store(text => $rttext);
 print "New ticket's ID is ", $ticket->id, "\n";
 
