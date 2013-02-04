2a4db869ebb7319975626ec92b19ea3d2f48f06e
unknown <cjshi@istl015.nexus.uwaterloo.ca>
2013-02-04 13:49:45 -0500

    combined tool for searching overdue tickets

--- bin/rt-overdue-invest.pl
+++ bin/rt-overdue-invest.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-#IST-ISS Co-op Cheng Jie Shi <cjshi@uwaterloo.ca> Jan 2013
+#IST-ISS Co-op Cheng Jie Shi <cjshi@uwaterloo.ca> Feb 2013
 #Supervisor: Mike Patterson <mike.patterson@uwaterloo.ca>
 
 use strict;
@@ -10,11 +10,14 @@ use lib "$FindBin::Bin/../lib";
 
 use Data::Dumper;
 use RT::Client::REST;
-use RT::Client::REST::Ticket;
 use Error qw|:try|;
 use Date::Manip;
 use ConConn;
+use vars qw/ $opt_s $opt_e $opt_r $opt_E $opt_d/;
+use Getopt::Std;
 
+getopts('s:e:r:Ed');
+my $start_run = time();
 my $debug = 0;
 
 my %config = ISSRT::ConConn::GetConfig();
@@ -32,11 +35,42 @@ try {
 
 my $tday = UnixDate("today","%Y-%m-%d");
 
-my $qstring = qq|
-Queue = 'Investigations'
-AND CF.{_RTIR_State} = 'open'
-AND Due < '$tday'
-|;
+my($lm,$nm,$qstring);
+
+
+#-s -e time options
+if($opt_s && $opt_e){
+  $lm = $opt_s;
+  $nm = $opt_e;
+  $qstring = qq|
+    Queue = 'Investigations'
+    AND Created > '$lm'
+    AND Created < '$nm'
+    AND CF.{_RTIR_State} = 'open'
+    AND Due < '$tday'
+    |;
+    print "Overdue Investigations from $lm to $nm\n";
+}elsif ($opt_s && (!$opt_e)){
+  $lm = $opt_s;
+  $nm = UnixDate("today","%Y-%m-%d");
+  $qstring = qq|
+    Queue = 'Investigations'
+    AND Created > '$lm'
+    AND Created < '$nm'
+    AND CF.{_RTIR_State} = 'open'
+    AND Due < '$tday'
+    |;
+    print "Overdue Investigations from $lm to $nm\n";
+}elsif ((!$opt_s) && (!$opt_e)){
+  $lm = $opt_s;
+  $nm = UnixDate("today","%Y-%m-%d");
+  $qstring = qq|
+    Queue = 'Investigations'
+    AND CF.{_RTIR_State} = 'open'
+    AND Due < '$tday'
+    |;
+    print "Overdue Investigations\n";
+}
 
 if($debug > 0){ print "Query string\n$qstring\n"; }
 
@@ -48,9 +82,45 @@ my @ids = $rt->search(
 if($debug > 0){	print scalar @ids . " investigations\n"; }
 if($debug > 1){	print Dumper(@ids); }
 
-print "Overdue Investigations\n";
-
-for my $id (@ids) {
+my @list;
+#-E send Emails
+if($opt_E && (!$opt_r)){
+  for my $id (@ids) {
+	my @corrspd;
+	my ($ticket) = $rt->show(type=>'ticket',id=>$id);
+	my (@parent_id) = $rt->get_transaction_ids(parent_id => $id);
+        foreach my $ele (@parent_id){
+	  	my $name = $rt->get_transaction (parent_id => $id, id => $ele);
+	  	my @des = $name -> {'Description'};
+	  	if ($des[0] =~ /^Correspondence/){
+	  	push(@corrspd, $des[0]);
+		}	
+	}
+	my $lastperson = $corrspd[$#corrspd];
+	if($debug > 0) {
+		print Dumper($ticket);
+	}
+	if(@corrspd){
+	   my $lastperson = $corrspd[$#corrspd];
+	   my $last = substr $lastperson, 24;
+	   if( ($last ne "mpatters" ) && ($last ne "issminion") ){
+	      push (@list, $id);
+            }
+        }
+        }  
+   #-E send emails
+   # foreach my $overdue (@list){
+   	 # my $msg = $rt->correspond(
+                    # ticket_id   => $overdue,
+                    # message     => "This is a test to see if correspondence has been added to RT, and RT sends an Email",
+                    # );
+   # }
+    print "Emails Sent"                  
+}	
+else{   
+  for my $id (@ids){
+    if($opt_r && (!$opt_E)){
+     if ($id == $opt_r){
 	my @corrspd;
 	my ($ticket) = $rt->show(type=>'ticket',id=>$id);
 	my (@parent_id) = $rt->get_transaction_ids(parent_id => $id);
@@ -76,7 +146,47 @@ for my $id (@ids) {
         }else{
            printf "%-10s %-12s %-12s %-30s %-30s\n",	
            $id,$owner,$owner,$ddate,$subj;
+         }
+       }
+     }elsif($opt_r && $opt_E){
+      	 if ($id == $opt_r){
+            # my $msg = $rt->correspond(
+                           # ticket_id   => $opt_r,
+                           # message     => "This is a test to see if correspondence has been added to RT, and RT sends an Email",
+                           # );
+      		 print"Email Sent\n"; 	
+      	 }
+      }
+      else{
+        my @corrspd;
+	my ($ticket) = $rt->show(type=>'ticket',id=>$id);
+	my (@parent_id) = $rt->get_transaction_ids(parent_id => $id);
+        foreach my $ele (@parent_id){
+	  	my $name = $rt->get_transaction (parent_id => $id, id => $ele);
+	  	my @des = $name -> {'Description'};
+	  	if ($des[0] =~ /^Correspondence/){
+	  	push(@corrspd, $des[0]);
+		}	
+	}
+	my $lastperson = $corrspd[$#corrspd];
+	if($debug > 0) {
+		print Dumper($ticket);
+	}
+	my $subj = $ticket->{'Subject'};
+	my $ddate = $ticket->{'Due'};
+	my $owner = $ticket->{'Owner'};
+	if(@corrspd){
+	   my $lastperson = $corrspd[$#corrspd];
+	   my $last = substr $lastperson, 24;
+	   printf "%-10s %-12s %-12s %-30s %-30s\n",	
+           $id,$owner,$last,$ddate,$subj;
+        }else{
+           printf "%-10s %-12s %-12s %-30s %-30s\n",	
+           $id,$owner,$owner,$ddate,$subj;
+           } 	
+        	
         }
+     }   
 }
 
 $qstring = qq|
@@ -91,4 +201,11 @@ for my $id (@ids) {
 	}
 	my $subj = $ticket->{'Subject'};
 	my $owner = $ticket->{'Owner'};
+}
+
+#-d option
+my $end_run = time();
+if($opt_d){
+  my $run_time = $end_run - $start_run;   
+  print  "Query took $run_time seconds\n"; 
 }
