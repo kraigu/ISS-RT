ea1cdc70aaf39c07f8e979030bdd68301422ffae
Mike Patterson <mike@snowcrash.ca>
2013-02-21 17:56:35 -0500

    start linting the code

--- bin/rt-overdue-invest.pl
+++ bin/rt-overdue-invest.pl
@@ -18,10 +18,15 @@ use Getopt::Std;
 
 getopts('s:e:r:f:v:Eh');
 
+my $overduemessage = qq|
+Please note that no correspondence has been received for this ticket lately,
+and that it is now overude.
+|;
+
 my $debug = $opt_v || 0;
 my %config;
 if($opt_h){
-   print "Options: -s(start-time), -e(end-time), -r(rearch for this ticker ID), -E(Send Emails to correspondence), -f(config file), -v(debug)\n";
+   print "Options: -s(start-time), -e(end-time), -r(report on ticket ID), -E(Send Emails to correspondents), -f(config file), -v(debug)\n";
    print "If only -r is given, report for the ticket ID. If -r and -E are given, send Email for the ticket ID\n";
 }else{
 if($opt_f){
@@ -92,76 +97,75 @@ my @list;
 #-E send Emails
 if($opt_E && (!$opt_r)){
   for my $id (@ids) {
-	my @corrspd;
-	my ($ticket) = $rt->show(type=>'ticket',id=>$id);
-	my (@parent_id) = $rt->get_transaction_ids(parent_id => $id);
-        foreach my $ele (@parent_id){
-	  	my $name = $rt->get_transaction (parent_id => $id, id => $ele);
+  	my @corrspd;
+  	my ($ticket) = $rt->show(type=>'ticket',id=>$id);
+  	my (@parent_id) = $rt->get_transaction_ids(parent_id => $id);
+    foreach my $ele (@parent_id){
+      my $name = $rt->get_transaction (parent_id => $id, id => $ele);
 	  	my @des = $name -> {'Description'};
 	  	if ($des[0] =~ /^Correspondence/){
-	  	push(@corrspd, $des[0]);
-		}	
-	}
-	my $lastperson = $corrspd[$#corrspd];
-	if($debug > 0) {
-		print Dumper($ticket);
-	}
-	if(@corrspd){
-	   my $lastperson = $corrspd[$#corrspd];
-	   my $last = substr $lastperson, 24;
-	   if( ($last ne "mpatters" ) && ($last ne "issminion") ){
-	      push (@list, $id);
-            }
-        }
-        }  
-   #-E send emails
-   # foreach my $overdue (@list){
-   	 # my $msg = $rt->correspond(
-                    # ticket_id   => $overdue,
-                    # message     => "This is a test to see if correspondence has been added to RT, and RT sends an Email",
-                    # );
-   # }
-    print "Emails Sent"                  
+	  	  push(@corrspd, $des[0]);
+		  }	
+    }
+  	my $lastperson = $corrspd[$#corrspd];
+  	if($debug > 0) {
+  		print Dumper($ticket);
+  	}
+  	if(@corrspd){
+  	   my $lastperson = $corrspd[$#corrspd];
+  	   my $last = substr $lastperson, 24;
+  	   if( ($last ne "mpatters" ) && ($last ne "issminion") ){ # hardcoded lists of users are evil
+  	     push (@list, $id);
+       }
+    }
+  }  
+  foreach my $overdue (@list){
+    my $msg = $rt->correspond(
+      ticket_id   => $overdue,
+      message     => $overduemessage,
+    );
+  }
+  if($debug > 0){
+    print "Emails Sent";
+  }
 }	
-else{   
+else {   
   for my $id (@ids){
     if($opt_r && (!$opt_E)){
-     if ($id == $opt_r){
-	my @corrspd;
-	my ($ticket) = $rt->show(type=>'ticket',id=>$id);
-	my (@parent_id) = $rt->get_transaction_ids(parent_id => $id);
+      if ($id == $opt_r){
+        my @corrspd;
+        my ($ticket) = $rt->show(type=>'ticket',id=>$id);
+        my (@parent_id) = $rt->get_transaction_ids(parent_id => $id);
         foreach my $ele (@parent_id){
-	  	my $name = $rt->get_transaction (parent_id => $id, id => $ele);
-	  	my @des = $name -> {'Description'};
-	  	if ($des[0] =~ /^Correspondence/){
-	  	push(@corrspd, $des[0]);
-		}	
-	}
-	my $lastperson = $corrspd[$#corrspd];
-	if($debug > 0) {
-		print Dumper($ticket);
-	}
-	my $subj = $ticket->{'Subject'};
-	my $ddate = $ticket->{'Due'};
-	my $owner = $ticket->{'Owner'};
-	if(@corrspd){
-	   my $lastperson = $corrspd[$#corrspd];
-	   my $last = substr $lastperson, 24;
-	   printf "%-10s %-12s %-12s %-30s %-30s\n",	
-           $id,$owner,$last,$ddate,$subj;
-        }else{
-           printf "%-10s %-12s %-12s %-30s %-30s\n",	
-           $id,$owner,$owner,$ddate,$subj;
-         }
-       }
-     }elsif($opt_r && $opt_E){
-      	 if ($id == $opt_r){
-            # my $msg = $rt->correspond(
-                           # ticket_id   => $opt_r,
-                           # message     => "This is a test to see if correspondence has been added to RT, and RT sends an Email",
-                           # );
-      		 print"Email Sent\n"; 	
-      	 }
+    	  	my $name = $rt->get_transaction (parent_id => $id, id => $ele);
+    	  	my @des = $name -> {'Description'};
+    	  	if ($des[0] =~ /^Correspondence/){
+            push(@corrspd, $des[0]);
+          }	
+        }
+        my $lastperson = $corrspd[$#corrspd];
+        if($debug > 0) {
+    		  print Dumper($ticket);
+        }
+      	my $subj = $ticket->{'Subject'};
+      	my $ddate = $ticket->{'Due'};
+      	my $owner = $ticket->{'Owner'};
+      	if(@corrspd){
+          my $lastperson = $corrspd[$#corrspd];
+      	  my $last = substr $lastperson, 24;
+      	  printf "%-10s %-12s %-12s %-30s %-30s\n",	$id,$owner,$last,$ddate,$subj;
+        } else {
+          printf "%-10s %-12s %-12s %-30s %-30s\n",	$id,$owner,$owner,$ddate,$subj;
+        }
+      }
+    } elsif($opt_r && $opt_E){
+        if ($id == $opt_r){
+          # my $msg = $rt->correspond(
+            # ticket_id   => $opt_r,
+            # message     => $overduemessage,
+          # );
+      		 print "Email Sent\n"; 	
+      	}
       }
       else{
         my @corrspd;
