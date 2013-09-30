1ebe8a79f66d494dda94daa2ef287a1efd68741f
davidsonmarshall <davidsonmarshall@gmail.com>
2013-09-30 16:08:36 -0400

    Fixes to rt-rep.pl

--- bin/rt-rep.pl
+++ bin/rt-rep.pl
@@ -29,7 +29,7 @@ my $debug = $opt_v || 0;
 
 my ($ticket,$checkmonth);
 my (%classifications,%constituencies,%config);
-my($lm,$nm,$qstring);
+my($lm,$nm);
 
 if($opt_f){
 	%config = ISSRT::ConConn::GetConfig($opt_f);
@@ -39,15 +39,24 @@ if($opt_f){
 
 #-s -e time options
 if($opt_s && $opt_e){
-  $lm = $opt_s;
-  $nm = $opt_e	
+	$lm = $opt_s;
+	$nm = $opt_e	
 }elsif ($opt_s && (!$opt_e)){
-  $lm = $opt_s;
-  $nm = UnixDate("today","%Y-%m-%d");
-}elsif((!$opt_s) && (!$opt_e)){
- $lm = UnixDate("-1m","%Y-%m-01");
- $nm = UnixDate("today","%Y-%m-01");
+	$lm = $opt_s;
+	$nm = UnixDate("today","%Y-%m-%d");
+}elsif((!$opt_s) && (!$opt_e)){	
+	$lm = UnixDate("-1m","%Y-%m-01");
+	$nm = UnixDate("today","%Y-%m-01");
 }
+
+my $qstring = qq|
+Queue = 'Incidents'
+AND Created > '$lm'
+AND Created < '$nm'
+AND CF.{_RTIR_Classification} != 'Question Only'
+AND CF.{_RTIR_Resolution} != 'abandoned'
+AND Status != 'rejected'
+|;
 
 my $rt = RT::Client::REST->new(
 	server => 'https://' . $config{hostname},
@@ -61,121 +70,39 @@ try {
 };
 
 if($opt_h){
-    print "Available options: -s (start-date),  -e (end-date), -c (include Copyright), -l (include LE request), -C (Copyright only), -L (LE request only), -V (verbose report), -v (enable debugging)\n";
-    print "If no dates are given, assume the previous calendar month. If only a start date is given, assume the current date as the end date\n";
-    exit 0;
-}
-#-V CSV output from weeklyrep.pl
-if($opt_V){
-  if($opt_l && (!$opt_c) && (!$opt_L) && (!$opt_C)){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND (
- 	CF.{_RTIR_Resolution} != 'abandoned' 
- 	OR
-	CF.{_RTIR_Classification} != 'Question Only'
-	OR
-	CF.{_RTIR_Status} != 'rejected'
-  )
-  AND CF.{_RTIR_Classification} != 'Copyright'
-|; 
-}#-c include 'Copyright'
-elsif((!$opt_l) && $opt_c && (!$opt_L) && (!$opt_C)){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND (
- 	CF.{_RTIR_Resolution} != 'abandoned' 
- 	OR
-	CF.{_RTIR_Classification} != 'Question Only'
-	OR
-	CF.{_RTIR_Status} != 'rejected'
-  )
-  AND CF.{_RTIR_Classification} != 'LE request'
-|;
-}#-c -l include 'Copyright' and 'LE Request'
-elsif($opt_l && $opt_c && (!$opt_L) && (!$opt_C)){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND (
- 	CF.{_RTIR_Resolution} != 'abandoned' 
- 	OR
-	CF.{_RTIR_Classification} != 'Question Only'
-	OR
-	CF.{_RTIR_Status} != 'rejected'
-  )
-|; 
-}#ouput for no options given. default to print anything other than 'LE Request' and 'Copyright'
-elsif((!$opt_l) && (!$opt_c) && (!$opt_L) && (!$opt_C)){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND (
- 	CF.{_RTIR_Resolution} != 'abandoned' 
- 	OR
-	CF.{_RTIR_Classification} != 'Question Only'
-	OR
-	CF.{_RTIR_Status} != 'rejected'
-  )
-  AND CF.{_RTIR_Classification} != 'LE request'
-  AND CF.{_RTIR_Classification} != 'Copyright'
-|;
-}#-L include ONLY 'LE Request'
-elsif((!$opt_l) && (!$opt_c) && $opt_L && (!$opt_C)){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND (
- 	CF.{_RTIR_Resolution} != 'abandoned' 
- 	OR
-	CF.{_RTIR_Classification} != 'Question Only'
-	OR
-	CF.{_RTIR_Status} != 'rejected'
-  )
-  AND CF.{_RTIR_Classification} = 'LE request'
-|;
-}#-C include ONLY 'Copyright'
-elsif((!$opt_l) && (!$opt_c) && (!$opt_L) && $opt_C){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND (
- 	CF.{_RTIR_Resolution} != 'abandoned' 
- 	OR
-	CF.{_RTIR_Classification} != 'Question Only'
-	OR
-	CF.{_RTIR_Status} != 'rejected'
-  )
-  AND CF.{_RTIR_Classification} = 'Copyright'
-|;
-}# -C -L
-elsif((!$opt_l) && (!$opt_c) && $opt_L && $opt_C){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND (
- 	CF.{_RTIR_Resolution} != 'abandoned' 
- 	OR
-	CF.{_RTIR_Classification} != 'Question Only'
-	OR
-	CF.{_RTIR_Status} != 'rejected'
-  )
-  AND (CF.{_RTIR_Classification} = 'LE request'
-       OR
-       CF.{_RTIR_Classification} = 'Copyright'
-       )     
-|;
+	print "Available options: -s (start-date),  -e (end-date), -c (include Copyright), -l (include LE request), -C (Copyright only), -L (LE request only), -V (verbose report), -v (enable debugging)\n";
+	print "If no dates are given, assume the previous calendar month. If only a start date is given, assume the current date as the end date\n";
+	exit 0;
 }
 
+#-l include 'LE Request'
+if($opt_l && (!$opt_c) && (!$opt_L) && (!$opt_C)){
+	$qstring .= qq|AND CF.{_RTIR_Classification} != 'Copyright'|; 
+}#-c include 'Copyright'
+elsif((!$opt_l) && $opt_c && (!$opt_L) && (!$opt_C)){
+	$qstring .= qq|AND CF.{_RTIR_Classification} != 'LE request'|;
+}#-c -l include 'Copyright' and 'LE Request'
+elsif($opt_l && $opt_c && (!$opt_L) && (!$opt_C)){
+	#do nothing
+}#ouput for no options given. default to print anything other than 'LE Request' and 'Copyright'
+elsif((!$opt_l) && (!$opt_c) && (!$opt_L) && (!$opt_C)){
+	$qstring .= qq|AND CF.{_RTIR_Classification} != 'LE request' AND CF.{_RTIR_Classification} != 'Copyright'|;
+}#-L include ONLY 'LE Request'
+elsif((!$opt_l) && (!$opt_c) && $opt_L && (!$opt_C)){
+	$qstring .= qq|AND CF.{_RTIR_Classification} = 'LE request'|;
+}#-C include ONLY 'Copyright'
+elsif((!$opt_l) && (!$opt_c) && (!$opt_L) && $opt_C){
+	$qstring .= qq|AND CF.{_RTIR_Classification} = 'Copyright'|;
+}#-C -L
+elsif((!$opt_l) && (!$opt_c) && $opt_L && $opt_C){
+ 	$qstring .= qq|
+AND(
+	CF.{_RTIR_Classification} = 'LE request'
+	OR
+	CF.{_RTIR_Classification} = 'Copyright'
+)
+|;
+}
 
 if($debug > 0){ print "Query string\n$qstring\n"; }
 
@@ -188,141 +115,49 @@ my @ids = $rt->search(
 if($debug > 0){	print scalar @ids . " incidents\n"; }
 if($debug > 1){	print Dumper(@ids); }
 
-print "RT Incident report for $lm to $nm\n";
-print qq|"RT number","Created",Classification","Constituency","Subject"\n|;
-
-for my $id (@ids) {
-	# show() returns a hash reference
-	my ($ticket) = $rt->show(type=>'ticket',id=>$id);
-	my $classif = $ticket->{'CF.{_RTIR_Classification}'} || "Unclassified";
-	my $constit = $ticket->{'CF.{_RTIR_Constituency}'};
-	my $subj = $ticket->{'Subject'};
-	my $tickdate = $ticket->{'Created'};
-	print qq|"$id","$tickdate","$classif","$constit","$subj"\n|;
-	if($debug > 1) {
-		print "DEBUG:\n";
-		print Dumper($ticket);
+if($opt_V) {
+	print "RT Incident report for $lm to $nm\n";
+	print qq|"RT number","Created",Classification","Constituency","Subject"\n|;
+	for my $id (@ids) {
+		# show() returns a hash reference
+		my ($ticket) = $rt->show(type=>'ticket',id=>$id);
+		my $classif = $ticket->{'CF.{_RTIR_Classification}'} || "Unclassified";
+		my $constit = $ticket->{'CF.{_RTIR_Constituency}'};
+		my $subj = $ticket->{'Subject'};
+		my $tickdate = $ticket->{'Created'};
+		print qq|"$id","$tickdate","$classif","$constit","$subj"\n|;
+		if($debug > 2) {
+			print "DEBUG:\n";
+			print Dumper($ticket);
+		}
 	}
-}		
-}else{
-#-l include 'LE Request'
-if($opt_l && (!$opt_c) && (!$opt_L) && (!$opt_C)){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND CF.{_RTIR_Classification} != 'Question Only'
-  AND CF.{_RTIR_Resolution} != 'abandoned'
-  AND CF.{_RTIR_Status} != 'rejected'
-  AND CF.{_RTIR_Classification} != 'Copyright'
-|; 
-}#-c include 'Copyright'
-elsif((!$opt_l) && $opt_c && (!$opt_L) && (!$opt_C)){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND CF.{_RTIR_Classification} != 'Question Only'
-  AND CF.{_RTIR_Resolution} != 'abandoned'
-  AND CF.{_RTIR_Status} != 'rejected'
-  AND CF.{_RTIR_Classification} != 'LE request'
-|;
-}#-c -l include 'Copyright' and 'LE Request'
-elsif($opt_l && $opt_c && (!$opt_L) && (!$opt_C)){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND CF.{_RTIR_Classification} != 'Question Only'
-  AND CF.{_RTIR_Resolution} != 'abandoned'
-  AND CF.{_RTIR_Status} != 'rejected'
-|; 
-}#ouput for no options given. default to print anything other than 'LE Request' and 'Copyright'
-elsif((!$opt_l) && (!$opt_c) && (!$opt_L) && (!$opt_C)){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND CF.{_RTIR_Classification} != 'Question Only'
-  AND CF.{_RTIR_Resolution} != 'abandoned'
-  AND CF.{_RTIR_Status} != 'rejected'
-  AND CF.{_RTIR_Classification} != 'LE request'
-  AND CF.{_RTIR_Classification} != 'Copyright'
-|;
-}#-L include ONLY 'LE Request'
-elsif((!$opt_l) && (!$opt_c) && $opt_L && (!$opt_C)){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND CF.{_RTIR_Classification} != 'Question Only'
-  AND CF.{_RTIR_Resolution} != 'abandoned'
-  AND CF.{_RTIR_Status} != 'rejected'
-  AND CF.{_RTIR_Classification} = 'LE request'
-|;
-}#-C include ONLY 'Copyright'
-elsif((!$opt_l) && (!$opt_c) && (!$opt_L) && $opt_C){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND CF.{_RTIR_Classification} != 'Question Only'
-  AND CF.{_RTIR_Resolution} != 'abandoned'
-  AND CF.{_RTIR_Status} != 'rejected'
-  AND CF.{_RTIR_Classification} = 'Copyright'
-|;
-}#-C -L
-elsif((!$opt_l) && (!$opt_c) && $opt_L && $opt_C){
-  $qstring = qq|
-  Queue = 'Incidents'
-  AND Created > '$lm'
-  AND Created < '$nm'
-  AND CF.{_RTIR_Classification} != 'Question Only'
-  AND CF.{_RTIR_Resolution} != 'abandoned'
-  AND CF.{_RTIR_Status} != 'rejected'
-  AND (CF.{_RTIR_Classification} = 'LE request'
-       OR
-       CF.{_RTIR_Classification} = 'Copyright'
-       )     
-|;
-}
-
-if($debug > 0){ print "Query string\n$qstring\n"; }
-
-my @ids = $rt->search(
-	type => 'ticket',
-	query => $qstring
-);
-
-if($debug > 0){	print scalar @ids . " incidents\n"; }
-if($debug > 1){	print Dumper(@ids); }
-
-for my $id (@ids) {
-	# show() returns a hash reference
-	my ($ticket) = $rt->show(type=>'ticket',id=>$id);
-	next if($ticket->{'CF.{_RTIR_State}'} eq 'abandoned'); # RT is stupid and SQL statement can't exclude state?
-	my $classkey = $ticket->{'CF.{_RTIR_Classification}'};
-	my $conskey = $ticket->{'CF.{_RTIR_Constituency}'};
-	$classifications{$classkey} ||= 0;
-	$classifications{$classkey} += 1; # hurray for mr cout
-	$constituencies{$conskey} ||= 0;
-	$constituencies{$conskey} += 1;
-	if($debug > 1){
-		print "$id\t$conskey\t$ticket->{'Subject'}\n";
+}else {		
+	for my $id (@ids) {
+		# show() returns a hash reference
+		my ($ticket) = $rt->show(type=>'ticket',id=>$id);
+		next if($ticket->{'CF.{_RTIR_State}'} eq 'abandoned'); # RT is stupid and SQL statement can't exclude state?
+		my $classkey = $ticket->{'CF.{_RTIR_Classification}'};
+		my $conskey = $ticket->{'CF.{_RTIR_Constituency}'};
+		$classifications{$classkey} ||= 0;
+		$classifications{$classkey} += 1; # hurray for mr cout
+		$constituencies{$conskey} ||= 0;
+		$constituencies{$conskey} += 1;
+		if($debug > 1){
+			print "$id\t$conskey\t$ticket->{'Subject'}\n";
+		}
+		if($debug > 2){
+			print Dumper($ticket);
+		}
 	}
-	if($debug > 2){
-		print Dumper($ticket);
-	}
-}
-
-print "RT Incident report for $lm to $nm\nClassifications\n";
-
-print "$classifications{$_}\t$_\n" for sort 
- { $classifications{$b} <=> $classifications{$a} } keys %classifications;
-
-print "\nConstituencies\n";
-
-print "$constituencies{$_}\t$_\n" for sort 
- { $constituencies{$b} <=> $constituencies{$a} } keys %constituencies;
+	
+	print "RT Incident report for $lm to $nm\nClassifications\n";
+	
+	print "$classifications{$_}\t$_\n" for sort 
+	 { $classifications{$b} <=> $classifications{$a} } keys %classifications;
+	
+	print "\nConstituencies\n";
+	
+	print "$constituencies{$_}\t$_\n" for sort 
+	 { $constituencies{$b} <=> $constituencies{$a} } keys %constituencies;
 }
 
