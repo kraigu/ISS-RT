b6b5c40a2cab41d84bb66a09c29029d439753bba
Mike Patterson <mike.patterson@uwaterloo.ca>
2015-03-20 08:32:10 -0400

    ignore freak reports

--- bin/rt-shadow-submit.pl
+++ bin/rt-shadow-submit.pl
@@ -36,7 +36,8 @@ my @noisysubjects = (
 	"Shadowserver University of Waterloo NTP Report",
 	"Shadowserver University of Waterloo NTP Version Report",
 	"Shadowserver University of Waterloo NTP Monitor Report",
-	"Shadowserver University of Waterloo SSLv3/Poodle Vulnerable Servers Report"
+	"Shadowserver University of Waterloo SSLv3/Poodle Vulnerable Servers Report",
+	"Shadowserver University of Waterloo SSL/Freak Vulnerable Servers Report"
 );
 
 if($opt_h) {
