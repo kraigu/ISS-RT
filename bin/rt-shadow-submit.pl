91327e05c6cf41a27244cd44c9c19d76e77ccbfa
Mike Patterson <mike.patterson@uwaterloo.ca>
2015-04-04 11:36:42 -0400

    ignore mongo too :(

--- bin/rt-shadow-submit.pl
+++ bin/rt-shadow-submit.pl
@@ -38,7 +38,8 @@ my @noisysubjects = (
 	"Shadowserver University of Waterloo NTP Monitor Report",
 	"Shadowserver University of Waterloo SSLv3/Poodle Vulnerable Servers Report",
 	"Shadowserver University of Waterloo SSL/Freak Vulnerable Servers Report",
-	"Shadowserver University of Waterloo scan_ssl_poodle"
+	"Shadowserver University of Waterloo scan_ssl_poodle",
+	"Shadowserver University of Waterloo Accessible/Open MongoDB Service Report"
 );
 
 if($opt_h) {
